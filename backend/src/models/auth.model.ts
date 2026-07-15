fe39a196fc73b2c0af6d14e1059dae9990b920c6
Md Musharraf <mdmusharraf41125@gmail.com>
2026-07-15 13:30:51 +0530

    working on auth

---
+++ backend/src/models/auth.model.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const authSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const authModel = mongoose.model("auth", authSchema);
+
+export default authModel;
--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,9 +1,31 @@
+import authModel from "../models/auth.model";
 import { RegisterUser } from "./../types/types";
 import express, { Request, Response } from "express";
+import bcrypt from "bcrypt";
+
 const router = express.Router();
 
 router.post("/auth/register-user", async (req: Request<{}, {}, RegisterUser>, res) => {
-  console.log(req.body);
+  const { name, email, password, confirmPassword } = req.body;
+
+  console.log(name, email, password, confirmPassword);
+
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Password do not match" });
+  }
+
+  // hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await authModel.findOne({ email });
+
+  if (existingUser) {
+    return res.status(409).json({ message: "User already exists" });
+  }
+
+  const newUser = await authModel.create({ name, email, password: hashedPassword });
+
+  await newUser.save();
 });
 
 export default router;
