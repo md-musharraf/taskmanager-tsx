4c3e1a8cfcb2cbad3a5035b701469de7ed90aca6
Md Musharraf <mdmusharraf41125@gmail.com>
2026-07-16 11:41:14 +0530

    add login system

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import authModel from "../models/auth.model";
 import { RegisterUser } from "./../types/types";
-import {LoginUser} from "./../types/types"
-import express, { Request, Response } from "express";
+import { LoginUser } from "./../types/types";
+import express, { Request } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -48,6 +48,37 @@ router.post("/auth/register-user", async (req: Request<{}, {}, RegisterUser>, re
   }
 });
 
-router.post("/auth/login-user", async(req:Request<{},{},>) );
+router.post("/auth/login-user", async (req: Request<{}, {}, LoginUser>, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await authModel.findOne({ email });
+    if (!existingUser) {
+      return res.status(404).json({ message: "Email not found" });
+    }
+
+    const verifyPassword = await bcrypt.compare(password, existingUser.password as string);
+
+    if (!verifyPassword) {
+      return res.status(401).json({
+        message: "Password do not match",
+      });
+    }
+
+    const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET_KEY as string, {
+      expiresIn: "7d",
+    });
+
+    const { password: _, ...user } = existingUser.toObject();
+
+    res.cookie("token", token, {}).status(200).json({ user: user, message: "Login successfully" });
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({ message: error.message });
+    } else {
+      return res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  }
+});
 
 export default router;
